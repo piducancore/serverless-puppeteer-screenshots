615404bd61621dedcf7c7f0208633b0bcbcb0949
Rodrigo Moreira <hola@piducan.dev>
2023-04-04 15:24:56 -0400

    create pdf

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import chromium from "chrome-aws-lambda";
 
 export default async function handler(req, res) {
-  const { url, as } = req.query;
+  const { url } = req.query;
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -13,21 +13,14 @@ export default async function handler(req, res) {
 
   let page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: "networkidle2" });
+  await page.goto(url, { waitUntil: "networkidle0" });
 
-  if (as === "pdf") {
-    const pdf = await page.pdf({ format: "A4" });
-    await browser?.close();
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Accept-Ranges", "bytes");
-    res.setHeader("Content-Disposition", `inline; filename=screenshot.pdf`);
-    res.send(imageBuffer);
-  } else {
-    const imageBuffer = await page.screenshot();
-    await browser?.close();
-    res.setHeader("Content-Type", "image/png");
-    res.setHeader("Accept-Ranges", "bytes");
-    res.setHeader("Content-Disposition", `inline; filename=screenshot.png`);
-    res.send(imageBuffer);
-  }
+  const pdf = await page.pdf({ format: "A4" });
+
+  await browser?.close();
+
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader("Accept-Ranges", "bytes");
+  res.setHeader("Content-Disposition", `inline; filename=file.pdf`);
+  res.send(pdf);
 }
